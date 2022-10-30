a15a6aa63afb93567dbb02d9f457f21c8430e4d9
belmonjd1 <belmonjd@mail.uc.edu>
2022-10-30 17:13:42 -0400

    Mapped endpoints for the CRUD methods; changed port number in application.properties file.

--- src/main/java/com/pantrypal/enterprise/PantryPalController.java
+++ src/main/java/com/pantrypal/enterprise/PantryPalController.java
@@ -1,7 +1,10 @@
 package com.pantrypal.enterprise;
 
+import com.pantrypal.enterprise.dto.Recipe;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PantryPalController {
@@ -13,5 +16,25 @@ public class PantryPalController {
     @RequestMapping("/")
     public String index() {
         return "start";
+    }
+
+    @GetMapping("/recipe")
+    public ResponseEntity fetchAllRecipes() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/recipe/{id}")
+    public ResponseEntity fetchRecipeById(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/recipe", consumes="application/json", produces="application/json")
+    public Recipe createRecipe(@RequestBody Recipe recipe) {
+        return recipe;
+    }
+
+    @DeleteMapping("/recipe/{id}/")
+    public ResponseEntity deleteRecipe(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
